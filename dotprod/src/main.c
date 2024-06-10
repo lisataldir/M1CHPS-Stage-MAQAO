ff2caebb51363a973618f5646f97548896cfa905
LisaTaldir <lisa.taldir@free.fr>
2024-06-10 15:18:22 +0200

    time measurement

--- dotprod/src/main.c
+++ dotprod/src/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+#include <time.h>
 
 int main(int argc, char** argv){
 
@@ -26,31 +27,62 @@ int main(int argc, char** argv){
 
     double ref, res_unroll4, res_unroll4_v2, res_unroll4_v3, res_mpi, res_omp;
 
+    clock_t t0, t1;
+    double *elapsed = (double *)malloc(7*sizeof(double));
+
     for(u64 i=0; i<r; ++i){
 
+        t0 = clock();
         vector_init(x, n);
         vector_init(y, n);
+        t1 = clock();
+        elapsed[0] += (t1-t0);
 
+        t0 = clock();
         ref = dotprod(x, y, n);
+        t1 = clock();
+        elapsed[1] += (t1-t0);
+
+        t0 = clock();
         res_unroll4 = dotprod_unroll4(x, y, n);
+        t1 = clock();
+        elapsed[2] += (t1-t0);
+
+        t0 = clock();
         res_unroll4_v2 = dotprod_unroll4_v2(x, y, n);
+        t1 = clock();
+        elapsed[3] += (t1-t0);
+
+        t0 = clock();
         res_unroll4_v3 = dotprod_unroll4_v3(x, y, n);
+        t1 = clock();
+        elapsed[4] += (t1-t0);
+
+        t0 = clock();
         res_mpi = dotprod_parallel_mpi(x, y, n, rank, size);
+        t1 = clock();
+        elapsed[5] += (t1-t0);
+
+        t0 = clock();
         res_omp = dotprod_parallel_omp(x, y, n);
+        t1 = clock();
+        elapsed[6] += (t1-t0);
 
     }
 
     if(rank == 0){
-        printf("Résultat séquentiel : %lf\n", ref);
-        printf("Résultat séquentiel avec UNROLL 4: %lf\n", res_unroll4);
-        printf("Résultat séquentiel avec UNROLL 4 v2: %lf\n", res_unroll4_v2);
-        printf("Résultat séquentiel avec UNROLL 4 v3: %lf\n", res_unroll4_v3);
-        printf("Résultat parallèle avec mpi: %lf\n", res_mpi);
-        printf("Résultat parallèle avec omp: %lf\n", res_omp);
+        printf("Temps initialisation : %1.2lf s\n", elapsed[0]/1000000);
+        printf("Résultat séquentiel : %lf en %1.2lf s\n", ref, elapsed[1]/1000000);
+        printf("Résultat séquentiel avec UNROLL 4 : %lf en %1.2lf s\n", res_unroll4, elapsed[2]/1000000);
+        printf("Résultat séquentiel avec UNROLL 4 v2 : %lf en %1.2lf s\n", res_unroll4_v2, elapsed[3]/1000000);
+        printf("Résultat séquentiel avec UNROLL 4 v3 : %lf en %1.2lf s\n", res_unroll4_v3, elapsed[4]/1000000);
+        printf("Résultat parallèle avec mpi: %lf en %1.2lf s\n", res_mpi, elapsed[5]/1000000);
+        printf("Résultat parallèle avec omp: %lf en %1.2lf s\n", res_omp, elapsed[6]/1000000);
     } 
 
     free(x);
     free(y);
+    free(elapsed);
 
     MPI_Finalize();
     
